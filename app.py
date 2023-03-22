4b701ceebc520bb7c7144b78ded5eb0938934f03
ahmedgamal8410088 <128539757+ahmedgamal8410088@users.noreply.github.com>
2023-03-22 03:39:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ with st.sidebar.expander(" 🛠️ Settings ", expanded=False):
 
 
 # Set up the Streamlit app layout
-st.title("🧠 Chat Bot 🤖 Powered by Ahmed Gamal")
+st.title("🧠 Chat Bot Powered by Ahmed Gamal🤖")
 # st.markdown(" > Powered by Ahmed Gamal")
 
 # Ask the user to enter their OpenAI API key
